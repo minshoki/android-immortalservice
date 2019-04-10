d44ca97c0b48f694c8b0d41e5d854d9b52725d29
minshoki <a01050335943@gmail.com>
2019-04-10 14:12:23 +0900

    bintray update

--- immortalservice/src/main/java/com/devshoki/immortalservice/Immortal.kt
+++ immortalservice/src/main/java/com/devshoki/immortalservice/Immortal.kt
@@ -1,9 +1,15 @@
 package com.devshoki.immortalservice
 
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
@@ -39,27 +45,83 @@ class Immortal(
     }
 }
 
+
+fun Context.makeEmptyNotificationBuilder(to: Class<*>): NotificationCompat.Builder {
+    val appInfo = packageManager.getApplicationInfo(
+            packageName,
+            PackageManager.GET_META_DATA
+    )
+    val bundle = appInfo.metaData
+    val channelId = if(bundle != null) {
+        bundle.getString("immortal_service_default_channel_id")
+    } else {
+        "immortal_default_channel_id"
+    }
+    val channelName = if(bundle != null) {
+        bundle.getString("immortal_service_default_channel_name")
+    } else {
+        "immortal_default_channel_name"
+    }
+
+
+    val builder = NotificationCompat.Builder(this, channelId)
+    builder.setSmallIcon(R.drawable.ic_stat_name)
+    builder.setContentTitle(null)
+    builder.setContentText(null)
+    val notificationIntent = Intent(this, to)
+    val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
+    builder.setContentIntent(pendingIntent)
+
+    val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        manager.createNotificationChannel(NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT))
+    }
+
+    return builder
+}
+
 fun Context.startImmortal(
         lifecycleOwner: LifecycleOwner,
         block: ImmortalBuilder.() -> Unit
 ): Intent? {
+    val builder = ImmortalBuilder()
+    builder.block()
+
+    RealImmortalService.IMMORTAL = builder.builder()
+
+    var intent  = if (RealImmortalService.SERVICE_INTENT == null) {
+        val tempIntent = Intent(this, RealImmortalService::class.java)
+        startService(tempIntent)
+        tempIntent
+    } else {
+        RealImmortalService.SERVICE_INTENT
+    }
 
     lifecycleOwner.lifecycle.addObserver(object : LifecycleObserver {
         @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
         fun onDestory() {
             Log.e("shokitest", "destory observer")
+            if(intent != null) {
+                stopService(intent)
+                intent = null
+            }
         }
     })
+    return intent
+}
 
+fun Context.startImmortal(
+        block: ImmortalBuilder.() -> Unit
+): Intent? {
     val builder = ImmortalBuilder()
     builder.block()
 
     RealImmortalService.IMMORTAL = builder.builder()
 
     return if (RealImmortalService.SERVICE_INTENT == null) {
-        val intent = Intent(this, RealImmortalService::class.java)
-        startService(intent)
-        intent
+        val tempIntent = Intent(this, RealImmortalService::class.java)
+        startService(tempIntent)
+        tempIntent
     } else {
         RealImmortalService.SERVICE_INTENT
     }
